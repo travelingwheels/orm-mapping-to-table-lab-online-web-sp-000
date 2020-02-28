f0711edfee26edfc6121c84563873f2e240fc440
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-28 15:06:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,8 +30,8 @@ class Student
       INSERT INTO songs (name, grade) 
       VALUES (?, ?)
       SQL
-      
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
   # Remember, you can access your database connection anywhere in this class
