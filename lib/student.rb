0f99a4f102e24f994c73cb955a62c8744a16499c
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-28 14:37:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,6 +19,10 @@ class Student
         SQL
     DB[:conn].execute(sql) 
   end
+  
+  def self.drop_table
+    
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
